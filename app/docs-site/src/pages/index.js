5b6964ac0634fbdf295f21c62ab3616bf1b88162
yyokobori <yu.yokobori.web@gmail.com>
2026-03-12 14:08:37 +0900

    chore(docs-site): src配下をgit管理下に追加

---
+++ app/docs-site/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Docusaurus!</h1>
+      <p>This is the home page.</p>
+    </main>
+  );
+}
